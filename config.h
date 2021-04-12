dae093b365a9b8e96d919bba3182f23df1a3f3de
= <=>
2021-04-12 22:25:38 +0100

    Found a big problem but it's late so gonna log it and fix it tomorrow

--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #define URLLEN 128 
-#define BASEURL "/home/hannah/repos/cite/docs"
-#define SRCDIR "/../src/"
+#define BASEURL "https://hannah-scott.github.io/cite/"
+#define SRCDIR "/src/"
 #define DESTDIR "/pages/"
 #define TITLE "Blog"
 #define CSS "/css/main.css"
